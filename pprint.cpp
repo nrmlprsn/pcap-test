e12bcbd2a27117a7cdd6a0aa3ebf0c7c02deb0fb
nrmlprsn <8624paul@gmail.com>
2026-03-29 03:15:32 -0400

    Edit pprint.cpp

--- pprint.cpp
+++ pprint.cpp
@@ -3,12 +3,12 @@
 #include <iostream>
 using namespace std;
 
-void mac_format(eth_hdr* eth){
-
+string mac_format(uint8_t* mac){
+	return format("%02x:%02x:%02x:%02x:%02x:%02x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
 }
 
-void ipv4_format(ipv4_hds* ip){
-
+string ipv4_format(uint32_t add){
+	
 }
 
 void print_info(eth_hdr* eth, ipv4_hdr* ip, tcp_hdr* tcp){
