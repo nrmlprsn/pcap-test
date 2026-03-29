5b0e308344c194f32b3e4e79626eda7ecd36cdbd
nrmlprsn <8624paul@gmail.com>
2026-03-29 03:04:35 -0400

    Add pprint.cpp

---
+++ pprint.cpp
@@ -0,0 +1,17 @@
+#include "hdr.h"
+#include "pprint.h"
+#include <iostream>
+using namespace std;
+
+void mac_format(eth_hdr* eth){
+
+}
+
+void ipv4_format(ipv4_hds* ip){
+
+}
+
+void print_info(eth_hdr* eth, ipv4_hdr* ip, tcp_hdr* tcp){
+
+}
+
