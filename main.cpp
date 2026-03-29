1dfe661ece7b7fbcb01db45e7c532ef47e2ae815
nrmlprsn <143049879+nrmlprsn@users.noreply.github.com>
2026-03-29 18:06:57 +0900

    Edit main.cpp

--- main.cpp
+++ main.cpp
@@ -46,20 +46,20 @@ int main(int argc, char* argv[]) {
                         break;
                 }
                 
-		eth_hdr* eth = (eth_hdr*)packet;
-		if(ntohs(eth->type) != 0x0800) continue;
+				eth_hdr* eth = (eth_hdr*)packet;
+				if(ntohs(eth->type) != 0x0800) continue;
+				
+				ipv4_hdr* ip = (ipv4_hdr*)(packet + sizeof(eth_hdr));
+				uint8_t ihl = ((ip->v_ihl) & 0x0f) << 2;
+				if(ip->protocol != 6) continue;
 		
-		ipv4_hdr* ip = (ipv4_hdr*)(packet + sizeof(eth_hdr));
-		uint8_t ihl = ((ip->v_ihl) & 0x0f) << 2;
-		if(ip->protocol != 6) continue;
-
-		tcp_hdr* tcp = (tcp_hdr*)((uint8_t*)ip + ihl);
-		uint8_t thl = (((tcp->off_res) >> 4) & 0x0f) << 2;
-
-		uint8_t* data = (uint8_t*)tcp + thl;
-		uint16_t data_len = (ntohs(ip->len) > ihl + thl) ? ntohs(ip->len) - ihl - thl : 0;
-
-		print_info(eth, ip, tcp, data, data_len);
+				tcp_hdr* tcp = (tcp_hdr*)((uint8_t*)ip + ihl);
+				uint8_t thl = (((tcp->off_res) >> 4) & 0x0f) << 2;
+		
+				uint8_t* data = (uint8_t*)tcp + thl;
+				uint16_t data_len = (ntohs(ip->len) > ihl + thl) ? ntohs(ip->len) - ihl - thl : 0;
+		
+				print_info(eth, ip, tcp, data, data_len);
         }
 
         pcap_close(pcap);
