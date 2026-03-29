78a48c58f42cae67505435cb779eb6ac4e263fcf
nrmlprsn <8624paul@gmail.com>
2026-03-29 03:03:57 -0400

    Add hdr.h

---
+++ hdr.h
@@ -0,0 +1,21 @@
+#include <stdint.h>
+
+typedef struct{
+	uint8_t dst[6];
+	uint8_t src[6];
+	uint16_t type;
+}eth_hdr;
+
+typedef struct{
+	uint8_t v_ihl;
+	uint8_t a[8];
+	uint8_t protocol;
+	uint16_t csum;
+	uint32_t src;
+	uint32_t dst;
+}ipv4_hdr;
+
+typedef struct{
+	uint16_t src;
+	uint16_t dst;
+}tcp_hdr;
